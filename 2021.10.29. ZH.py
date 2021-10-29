dc84a477b8b57a3eb6af65dfdef42c6d9000fda8
verebelyi15 <verebelyilaura15@gmail.com>
2021-10-29 16:27:54 +0200

    vege

--- 2021.10.29. ZH.py
+++ 2021.10.29. ZH.py
@@ -90,11 +90,11 @@ dijazas=sorted(dijazas, key=pontszam_szerint, reverse=True)
 
 dijazottak_szama=1
 helyezes=1
-print('{}. díj ({} pont): {}'.format(helyezes, dijazas[i][1], dijazas[i][0]))
+print("{}. díj ({} pont): {}".format(helyezes, dijazas[i][1], dijazas[i][0]))
 i=1
 while dijazottak_szama<4:
     if dijazas[i][1]!=dijazas[i-1][1]:
         helyezes+=1
-    print('{}. díj ({} pont): {}'.format(helyezes, dijazas[i][1], dijazas[i][0]))
+    print("{}. díj ({} pont): {}".format(helyezes, dijazas[i][1], dijazas[i][0]))
     dijazottak_szama+=1
     i+=1
